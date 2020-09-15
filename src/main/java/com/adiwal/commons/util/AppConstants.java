5cd08c50e4f9d37f2b6dd73b27f9c16b835f5abc
Vikas Adiwal <vikas.adiwal@qio.io>
2020-09-15 23:30:40 +0530

    initial commi

---
+++ src/main/java/com/adiwal/commons/util/AppConstants.java
@@ -0,0 +1,12 @@
+package com.adiwal.commons.util;
+
+public class AppConstants {
+
+    private AppConstants(){}
+
+    public static final String TOKEN_ID = "tokenId";
+    public static final String REFRESH_TOKEN = "refreshToken";
+    public static final String AUTHENTICATION_ID = "authenticationId";
+    public static final String CLIENT_ID = "clientId";
+    public static final String USER_NAME = "username";
+}
